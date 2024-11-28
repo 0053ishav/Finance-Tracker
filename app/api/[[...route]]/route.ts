2612fe12d8874dad167d5da953e955929a21b7e1
0007ishav <2002ishavs@gmail.com>
2024-11-28 20:38:57 +0530

    Add CORS

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
 import summary from './summary'
 import  accounts  from "./accounts";
@@ -9,6 +10,16 @@ import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+
+app.use(
+    "*",
+    cors({
+      origin: process.env.NEXT_PUBLIC_APP_URL || "*",
+      allowMethods: ["GET", "POST", "PATCH", "DELETE"],
+      allowHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+
 
 app.onError((err, c) => {
     if (err instanceof HTTPException) {
